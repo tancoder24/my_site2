c41b0b97f888a4212da7ac0d1eb80aa442e7d5bb
tancoder <tanmaygupta2408@gmail.com>
2023-03-01 18:19:02 +0530

    second commit

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.example_view),
+    path('variable/', views.variable_view)
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def example_view(request):
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+
+    my_var = {
+        "first_name": "Tanmay",
+        "last_name": "Gupta",
+        "some_list": [1,2,4],
+        "some_dict": {
+                "some_key": "some_value"
+            }
+    }
+
+    return render(request, 'my_app/variable.html',context=my_var)
