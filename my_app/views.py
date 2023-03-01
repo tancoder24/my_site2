3a61dc2b9139e5124aaab5cc570f13263496c271
tancoder <tanmaygupta2408@gmail.com>
2023-03-01 22:40:30 +0530

    tag and url name added

--- my_app/views.py
+++ my_app/views.py
@@ -6,13 +6,4 @@ def example_view(request):
 
 def variable_view(request):
 
-    my_var = {
-        "first_name": "Tanmay",
-        "last_name": "Gupta",
-        "some_list": [1,2,4],
-        "some_dict": {
-                "some_key": "some_value"
-            }
-    }
-
-    return render(request, 'my_app/variable.html',context=my_var)
+    return render(request, 'my_app/variable.html')
